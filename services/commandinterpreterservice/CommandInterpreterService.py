228a03987ca8d2898184e2fe987b5201acaffcd7
CydrickT <CydrickT@github.com>
2020-07-06 22:32:18 -0400

    WIP: Improvements to services

--- services/commandinterpreterservice/CommandInterpreterService.py
+++ services/commandinterpreterservice/CommandInterpreterService.py
@@ -1,18 +1,58 @@
+from typing import Final
+
 from core.Service import Service
 from topics.buttoninput.ButtonInputCommand import ButtonInputCommand
+from topics.buttoninput.ButtonInputType import ButtonInputType
+from topics.generalstate.GeneralStateChangeNotification import GeneralStateChangeNotification
 from topics.generalstate.GeneralStateType import GeneralStateType
+from topics.modifierstate.ModifierStateChangedNotification import ModifierStateChangedNotification
+from topics.modifierstate.ModifierType import ModifierType
 
 
 class CommandInterpreterService(Service):
+
+    def __init__(self, core):
+        self.core: Final = core
+        self.state = GeneralStateType.GetOutOfBed
+
+    def initialize(self):
+        self.core.dataRouter.subscribe(ButtonInputCommand(), self.handleButtonInput)
 
     # States:
     # 1- Going to sleep: Stop lights, start music
     # 2- True sleep: Close music
     # 3- Wakeup: Turn on lights, Change lights scene, wake computer up
-
-    def initialize(self):
-        self.core.dataRouter.subscribe(ButtonInputCommand(), self.handleButtonInput)
-        self.state = GeneralStateType.GetOutOfBed
-
     def handleButtonInput(self, buttonInput):
         if buttonInput.buttoninputype == ButtonInputType.UpLong:
+            self.state = self.getForwardMovingState(self.state)
+            self.core.dataRouter.publish(GeneralStateChangeNotification(self.state))
+        elif buttonInput.buttoninputype == ButtonInputType.DownLong:
+            self.state = self.getBackwardMovingState(self.state)
+            self.core.dataRouter.publish(GeneralStateChangeNotification(self.state))
+        elif buttonInput.buttoninputype == ButtonInputType.UpDownLong:
+            self.state = GeneralStateType.NightEmergency
+            self.core.dataRouter.publish(GeneralStateChangeNotification(self.state))
+        elif buttonInput.buttoninputype == ButtonInputType.UpShort:
+            self.dataRouter.publish(ModifierStateChangedNotification(ModifierType.Increase))
+        elif buttonInput.buttoninputype == ButtonInputType.DownShort:
+            self.dataRouter.publish(ModifierStateChangedNotification(ModifierType.Decrease))
+
+    def getForwardMovingState(self, previousState):
+        if previousState == GeneralStateType.GetOutOfBed:
+            return GeneralStateType.SleepPreparation
+        elif previousState == GeneralStateType.SleepPreparation:
+            return GeneralStateType.TrueSleep
+        elif previousState == GeneralStateType.TrueSleep:
+            return GeneralStateType.GetOutOfBed
+        elif previousState == GeneralStateType.NightEmergency:
+            return GeneralStateType.TrueSleep
+
+    def getBackwardMovingState(self, previousState):
+        if previousState == GeneralStateType.GetOutOfBed:
+            return GeneralStateType.TrueSleep
+        elif previousState == GeneralStateType.SleepPreparation:
+            return GeneralStateType.GetOutOfBed
+        elif previousState == GeneralStateType.TrueSleep:
+            return GeneralStateType.SleepPreparation
+        elif previousState == GeneralStateType.NightEmergency:
+            return GeneralStateType.TrueSleep
--- services/wakeonlanmanagerservice/WakeOnLanManagerService.py
+++ services/wakeonlanmanagerservice/WakeOnLanManagerService.py
@@ -1,4 +1,5 @@
 import configparser
+import json
 
 from core.Service import Service
 import struct
@@ -9,17 +10,19 @@ from topics.generalstate.GeneralStateType import GeneralStateType
 
 class WakeOnLanManagerService(Service):
 
-    def WakeOnLanManagerService(self):
-        self.config = configparser.ConfigParser()
-        self.config.read('WakeOnLanManagerService.config')
+    def __init__(self, core):
+        config = configparser.ConfigParser()
+        config.read('WakeOnLanManagerService.config')
+        self.computersToWake = json.loads(config['ServiceSpecific']['computersToWake'])
+        self.wakeOnLanPort = int(config['ServiceSpecific']['wakeOnLanPort'])
 
     def initialize(self):
         self.core.dataRouter.subscribe(GeneralStateChangeNotification(), self.handleStateChangeNotification)
 
     def handleStateChangeNotification(self, stateChangeNotification):
         if stateChangeNotification == GeneralStateType.GetOutOfBed:
-
-
+            for computerToWake in self.computersToWake:
+                self.wakeOnLan(computerToWake['macAddressToWake'], computerToWake['broadcastAddress'])
 
     def wakeOnLan(self, mac_address, broadcast_ip_address):
         # Construct 6 byte hardware address
@@ -36,5 +39,5 @@ class WakeOnLanManagerService(Service):
 
         soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         soc.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        soc.sendto(msg, (broadcast_ip_address, self.config.))
+        soc.sendto(msg, (broadcast_ip_address, self.wakeOnLanPort))
         soc.close()
