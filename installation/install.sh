6b0ef56ba4f5fbe141c838a5522a8830c0803c64
Cydrick Trudel <cydtru@hotmail.com>
2020-12-26 22:04:07 -0500

    Added script to auto start app

--- installation/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get dist-upgrade
-apt-get install alsa-utils
-sync
-modprobe snd_bcm2835
-amixer cset numid=3 1
---
+++ setup/setup_startup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Installation script for Home Automation
+# Sets up a Systemd start script for the Home Automation python program
+# Usage: sudo install_startup.sh
+
+CONFIGURATION_FILE_NAME="HomeAutomation.config"
+SYSTEMD_FILES_DESTINATION_PATH="/lib/systemd/system"
+
+if (( $EUID != 0 )) ; then
+    echo "Execute this script with sudo permissions. Exiting..."
+    exit
+fi
+
+ROOT_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
+PYTHON_LOC="$(which python3)"
+
+APP_DIR="$ROOT_DIR"
+echo "Enter the path of the application directory [$APP_DIR]:"
+read INPUT_APP_DIR
+if [ "$INPUT_APP_DIR" != "" ] ; then
+    APP_DIR=$INPUT_APP_DIR
+fi
+echo "Selected application directory: $APP_DIR"
+
+CONFIG_FILE="$APP_DIR/$CONFIGURATION_FILE_NAME"
+echo "Enter the path of the configuration file [$CONFIG_FILE]"
+read INPUT_CONFIG_FILE
+if [ "$INPUT_CONFIG_FILE" != "" ] ; then
+    CONFIG_FILE=$INPUT_CONFIG_FILE
+fi
+echo "Selected configuration file: $CONFIG_FILE"
+
+rm -rf "$SYSTEMD_FILES_DESTINATION_PATH/homeautomation.service"
+cp "$ROOT_DIR/setup/resources/homeautomation.service" "$SYSTEMD_FILES_DESTINATION_PATH"
+sed -i -e "s#PYTHON_LOC#$PYTHON_LOC#g" "$SYSTEMD_FILES_DESTINATION_PATH/homeautomation.service"
+sed -i -e "s#APP_DIR#$APP_DIR#g" "$SYSTEMD_FILES_DESTINATION_PATH/homeautomation.service"
+sed -i -e "s#CONFIG_FILE#$CONFIG_FILE#g" "$SYSTEMD_FILES_DESTINATION_PATH/homeautomation.service"
+echo "Copied file to $SYSTEMD_FILES_DESTINATION_PATH/homeautomation.service"
+
+systemctl daemon-reload
+systemctl enable homeautomation.service
+
+echo "Setup homeautomation.service to start upon system startup."
